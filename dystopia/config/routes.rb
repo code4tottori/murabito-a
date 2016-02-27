a06777c8d71971ed7a57dd0f449ab2ee0c4cbd4b
sakaguchi <takashi.sakaguchi@ummm.info>
2016-02-27 16:19:51 +0900

    レイアウト付けた

--- dystopia/config/routes.rb
+++ dystopia/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :events
   resources :carees
 
-  get '/', :to => redirect('/googlemap_marker.html')
+  # get '/', :to => redirect('/googlemap_marker.html')
   resources :mqtturis, only:[:index], path:'mqtturi'
-  # root to: 'welcome#index'
+  root to: 'welcome#index'
 end
