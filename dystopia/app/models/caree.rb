98555503b9251c8f3162a09e74a62e20a2512fbb
sakaguchi <takashi.sakaguchi@ummm.info>
2016-02-27 13:24:44 +0900

    hamajyotan's first commit

--- dystopia/app/models/caree.rb
+++ dystopia/app/models/caree.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: carees
+#
+#  id         :integer          not null, primary key
+#  name       :string(192)      not null
+#  icon       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_carees_on_name  (name)
+#
+
 class Caree < ActiveRecord::Base
   has_many :event
 
--- dystopia/app/models/event.rb
+++ dystopia/app/models/event.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id             :integer          not null, primary key
+#  caree_id       :integer
+#  event          :string(36)       not null
+#  heartrate      :float
+#  latitude       :float
+#  longitude      :float
+#  altitude       :float
+#  acceleration_x :float
+#  acceleration_y :float
+#  acceleration_z :float
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+# Indexes
+#
+#  index_events_on_event  (event)
+#
+
 class Event < ActiveRecord::Base
 end
