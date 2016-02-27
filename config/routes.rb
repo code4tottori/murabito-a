e53bcb06268c9973718c3ca71c201a00ed581cf2
sakaguchi <takashi.sakaguchi@ummm.info>
2016-02-28 00:33:02 +0900

    routes整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
   resources :events
   resources :carees do
@@ -8,7 +7,6 @@ Rails.application.routes.draw do
     end
   end
 
-  # get '/', :to => redirect('/googlemap_marker.html')
   resources :mqtturis, only:[:index], path:'mqtturi'
   root to: 'welcome#index'
 end
