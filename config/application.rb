aa84ccde5933f68cfa90b032afe2104d0226304b
sakaguchi <takashi.sakaguchi@ummm.info>
2016-03-05 11:23:53 +0900

    とっとぴあ

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require(platform)
 
 require_relative '../lib/sigV4utilsForAwsIoT'
 
-module Dystopia
+module Tottopia
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
